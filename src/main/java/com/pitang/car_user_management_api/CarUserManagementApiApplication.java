af00c92277c6d361e799aaca685184cc20d3512c
Natanael Rolim <natanael.10@hotmail.com>
2024-07-02 22:05:34 -0300

    first commit

---
+++ src/main/java/com/pitang/car_user_management_api/CarUserManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.pitang.car_user_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarUserManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarUserManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pitang/car_user_management_api/CarUserManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitang.car_user_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarUserManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
